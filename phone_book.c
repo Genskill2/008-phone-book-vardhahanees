cfc3b9dc9fa6e8a22c71b5f38b9aea064273f9a1
Vardhah Anees <63852903+vardhahanees@users.noreply.github.com>
2021-07-07 14:26:25 +0400

    Update phone_book.c

--- phone_book.c
+++ phone_book.c
@@ -178,9 +178,11 @@ void add(char *name, char *phone) {
 void list(FILE *db_file) {
   entry *p = load_entries(db_file);
   entry *base = p;
+  int count = 0;
   while (p!=NULL) {
     printf("%-20s : %10s\n", p->name, p->phone);
     p=p->next;
+    count++;
   }
   /* TBD print total count */
   free_entries(base);
